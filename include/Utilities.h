c4b960e6f0ffbbbc061cd8b7ed5450e8f28faec1
christurgeon <turgec@rpi.edu>
2019-11-10 20:32:18 -0500

    Added watchlist file / parser

--- include/Utilities.h
+++ include/Utilities.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <fstream>
+#include <iostream>
 #include <math.h>
+
+// Path to file that holds stock watchlist
+const static std::string WATCHLIST = "../watchlist.txt"; 
 
 class Utilities 
 {
@@ -8,5 +13,25 @@ class Utilities
         static double delta(double open, double current)
         {
             return (current - open) / open * 100;
+        }
+
+        static std::vector<std::string> parseWatchlistFile()
+        {
+            std::ifstream infile(WATCHLIST);
+            std::vector<std::string> tickers;
+            if (infile.is_open())
+            {
+                std::string ticker;
+                while(getline(infile, ticker))
+                {
+                    tickers.push_back(ticker);
+                }
+                infile.close();
+            }
+            else
+            {
+                std::cerr << "Unable to open up file: " << WATCHLIST << std::endl;
+            }
+            return tickers;
         }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -23,15 +23,24 @@ int main()
         return EXIT_FAILURE;
     }
 
+    std::vector<std::string> tickers = Utilities::parseWatchlistFile();
+
+    std::cout << "\nTICKERS: ";
+    for (auto t : tickers)
+    {
+        std::cout << t + " ";
+    }
+    std::cout << std::endl << std::endl;
+
     std::unique_ptr<CurlLibrary> curl = std::make_unique<CurlLibrary>();
 
     std::string url1 = connection->GetQueryString_INTRADAY("TSLA", "1min");
     std::string url2 = connection->GetQueryString_GLOBALQUOTE("TSLA");
 
     auto data1 = curl->GET(url1);
-    std::cout << data1 << std::endl;
+    // std::cout << data1 << std::endl;
     auto data2 = curl->GET(url2);
-    std::cout << data2 << std::endl;
+    // std::cout << data2 << std::endl;
 
     return EXIT_SUCCESS;
 }
