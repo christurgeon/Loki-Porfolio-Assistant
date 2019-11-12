72365d3ab93f2a93a1da9c582e20488e54e54591
christurgeon <turgec@rpi.edu>
2019-11-11 21:01:13 -0500

    Added methods to retrieve more time series data

--- include/AlphaVantageConnection.h
+++ include/AlphaVantageConnection.h
@@ -24,11 +24,16 @@ class AlphaVantageConnection
         static AlphaVantageConnection* m_instance;
         std::string m_api_key = "XXX";
         static const std::string DEFAULT_URL; 
+        std::string TimeSeriesHelper(const std::string& series, const std::string& ticker, bool all_data) const;
 
     public:
         static AlphaVantageConnection* getInstance();
         void setApiKey(const std::string& api_key); 
         std::string getApiKey() const; 
-        std::string GetQueryString_INTRADAY(const std::string& ticker, const std::string& interval);
-        std::string GetQueryString_GLOBALQUOTE(const std::string& ticker);
+        std::string GetQueryString_TIMESERIESDAILY(const std::string& ticker, bool all_data) const;
+        std::string GetQueryString_TIMESERIESWEEKLY(const std::string& ticker, bool all_data) const;
+        std::string GetQueryString_TIMESERIESMONTHLY(const std::string& ticker, bool all_data) const;
+        std::string GetQueryString_INTRADAY(const std::string& ticker, const std::string& interval) const;
+        std::string GetQueryString_GLOBALQUOTE(const std::string& ticker) const;
+
 }; 
--- src/AlphaVantageConnection.cpp
+++ src/AlphaVantageConnection.cpp
@@ -25,17 +25,43 @@ std::string AlphaVantageConnection::getApiKey() const
     return m_api_key;
 }
 
-std::string AlphaVantageConnection::GetQueryString_INTRADAY(const std::string& ticker, const std::string& interval)
+std::string AlphaVantageConnection::TimeSeriesHelper(const std::string& series, const std::string& ticker, bool all_data) const
+{
+    std::string size = all_data ? "full" : "compact";
+    std::string url = AlphaVantageConnection::DEFAULT_URL
+        + "function="   + series    + "&"
+        + "symbol="     + ticker    + "&"
+        + "outputsize=" + size      + "&"
+        + "apikey="     + m_api_key + "&";
+    return url;
+}
+
+std::string AlphaVantageConnection::GetQueryString_TIMESERIESDAILY(const std::string& ticker, bool all_data) const
+{    
+    return TimeSeriesHelper("TIME_SERIES_DAILY", ticker, all_data);
+}
+
+std::string AlphaVantageConnection::GetQueryString_TIMESERIESWEEKLY(const std::string& ticker, bool all_data) const
+{
+    return TimeSeriesHelper("TIME_SERIES_WEEKLY", ticker, all_data);
+}
+
+std::string AlphaVantageConnection::GetQueryString_TIMESERIESMONTHLY(const std::string& ticker, bool all_data) const 
+{
+    return TimeSeriesHelper("TIME_SERIES_MONTHLY", ticker, all_data);
+}
+
+std::string AlphaVantageConnection::GetQueryString_INTRADAY(const std::string& ticker, const std::string& interval) const
 {
     std::string url = AlphaVantageConnection::DEFAULT_URL
         + "function=TIME_SERIES_INTRADAY&"
         + "symbol="   + ticker    + "&" 
-        + "interval=" + interval  + "&" + "datatype=json&"
+        + "interval=" + interval  + "&"
         + "apikey="   + m_api_key + "&";
     return url;
 }
 
-std::string AlphaVantageConnection::GetQueryString_GLOBALQUOTE(const std::string& ticker)
+std::string AlphaVantageConnection::GetQueryString_GLOBALQUOTE(const std::string& ticker) const 
 {
     std::string url = AlphaVantageConnection::DEFAULT_URL
         + "function=GLOBAL_QUOTE&"
@@ -43,6 +69,3 @@ std::string AlphaVantageConnection::GetQueryString_GLOBALQUOTE(const std::string
         + "apikey=" + m_api_key + "&";
     return url;
 }
-
-
-
