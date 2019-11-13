5a6124823c79629e9513825d938ac953b350417e
christurgeon <turgec@rpi.edu>
2019-11-12 20:26:28 -0500

    Extract keys from settings.json

--- src/alpaca/main.py
+++ src/alpaca/main.py
@@ -1,9 +1,26 @@
-#!/usr/bin/python
-
 import alpaca_trade_api as tradeapi
 
+import json
+import sys
+
+CONFIGURATION_FILE_PATH = "../../settings.json"
+
+
 if __name__ == "__main__":
-    
-    api = tradeapi.REST('<key_id>', '<secret_key>', api_version='v2') # or use ENV Vars shown below
+
+    key_id = secret_key = ""
+    try:
+        file = open(CONFIGURATION_FILE_PATH, "r")
+        data = json.load(file)
+        key_id = data["alpaca_public_key"]
+        secret_key = data["alpaca_private_key"] 
+    except OSError:
+        print("Could not open configuration file located at \"%s\"" % CONFIGURATION_FILE_PATH)
+        sys.exit()
+    except:
+        print("Could not access json data within \"%s\"" % CONFIGURATION_FILE_PATH)
+        sys.exit()
+
+    api = tradeapi.REST(key_id, secret_key, api_version='v2') 
     account = api.get_account()
-    api.list_positions()
+    # api.list_position
