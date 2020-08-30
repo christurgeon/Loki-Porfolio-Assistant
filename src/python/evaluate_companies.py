59a54834e632837231a1654f1e74de2f46921a36
Christopher Turgeon <46657395+christurgeon@users.noreply.github.com>
2020-08-30 11:18:07 -0400

    Update evaluate_companies.py

--- src/python/evaluate_companies.py
+++ src/python/evaluate_companies.py
@@ -16,54 +16,136 @@
 
 import json
 import logging
+import os
 import requests
 import sys
 from bs4 import BeautifulSoup
 
 
+SPDATA_FILE = "spdata50.json"
 SP_URL = "http://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 YAHOO_URL = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-detail?region=US&lang=en&symbol={ticker}"
 YAHOO_HEADER = {
     "x-rapidapi-host"   : "apidojo-yahoo-finance-v1.p.rapidapi.com",
-    "x-rapidapi-key"    : "YOUR-API-KEY"
+    "x-rapidapi-key"    : "adc7a591f8mshaa69426641b44d3p166187jsn8c68f3fd1488"
 }
 
 
-def download(sess):
-    """
-    @goal   | retrieve data for S&P 500 companies and dump to json
-    @param  | sess - requests.Session() object
-    @return | 0 if successful, -1 if not, can throw exception
-    """
 
-    logger.info("download() start retrieving S&P 500 list")
+def getSP500():
+    """
+    @goal   | retrieve list of all S&P 500 tickers
+    @return | a list of all S&P 500 tickers, can throw exception
+    """
+    logger.info("getSP500() start retrieving S&P 500 list")
     tickers = []
-    for i in range(3):
-        resp = requests.get(url=SP_URL)
-        if resp.status_code != 200:
-            logger.error("download() could not reach {} response.status_code = {}".format(resp.url, resp.status_code))
-            return -1
-        else:
-            break
+    resp = requests.get(url=SP_URL)
+    if resp.status_code != 200:
+        logger.error("getSP500() could not reach {} response.status_code = {}".format(resp.url, resp.status_code))
     soup = BeautifulSoup(resp.text, "lxml")
     table = soup.find("table", {"class" : "wikitable sortable"})
     for row in table.findAll("tr")[1:]:
         tickers.append(row.findAll("td")[0].text.strip())
     logger.info(tickers)
+    if len(tickers) < 500:
+        logging.error("Expected number of tickers not retrieved from {}".format(SP_URL))
+        raise Exception("Unable to retrieve S&P 500 ticker list...")
+    return tickers
 
+
+
+def download(sess, tickers):
+    """
+    @goal   | retrieve data for S&P 500 companies and dump to text file
+    @param  | sess - requests.Session() object
+    @param  | tickers - list of all S&P 500 tickers
+    @return | 0 if successful, -1 otherwise, can throw exception
+    """
     logger.info("download() start retrieving S&P 500 data")
-    spdata = ""
+    spdata = []
     for i, t in enumerate(tickers):
         try:
             resp = sess.get(url=YAHOO_URL.format(ticker=t), headers=YAHOO_HEADER)
             logger.info("{}\t{}\t\t| response.status_code = {}".format(i, t, resp.status_code))
-            spdata += resp.text
+            spdata.append(resp.text)
         except Exception as e:
             logger.exception("download() exception occurred for {}: [{}]".format(t, e))
-    with open("spdata.json", "w") as file:
-        logger.info("download() start writing data to spdata.json")
-        json.dump(spdata, file)
+    with open(SPDATA_FILE, "w") as file:
+        logger.info("download() start writing data to spdata.txt")
+        for i in spdata:
+            file.write(i)
     return 0
+
+
+
+def evaluate(tickers):
+    """
+    @goal   | score each S&P 500 company
+    @param  | tickers, list of S&P 500 tickers
+    @return | 0 if successful, -1 otherwise, can throw exception
+    """
+    spdict = {k: None for k in tickers}
+
+    with open(SPDATA_FILE, "r") as file:
+        while True:
+            line = file.readline()
+            if not line:
+                logging.info("Finished reading file...")
+                return 0
+            spdata = json.loads(line)
+
+            pegRatio = s["defaultKeyStatistics"]["pegRatio"]["raw"]
+            earningsQuarterlyGrowth = s["defaultKeyStatistics"]["earningsQuarterlyGrowth"]["raw"]
+            profitMargins = s["defaultKeyStatistics"]["profitMargins"]["raw"]
+            f2WeekChange = s["defaultKeyStatistics"]["52WeekChange"]["raw"]
+            priceToBook = s["defaultKeyStatistics"]["priceToBook"]["raw"]
+            forwardEps = s["defaultKeyStatistics"]["forwardEps"]["raw"]
+            trailingEps = s["defaultKeyStatistics"]["trailingEps"]["raw"]
+            forwardPE = s["defaultKeyStatistics"]["forwardPE"]["raw"]
+            # sector = s["summaryProfile"]["sector"]
+
+            profitable = pegRatio > 0 and earningsQuarterlyGrowth > 0 and profitMargins > .1 #and sector != "Financial Services"
+            if profitable:
+                print(s["symbol"])
+                spdict[s["symbol"]] = [s]
+
+            # Highly profitable but is also down
+            profitable = earningsQuarterlyGrowth > 0 and f2WeekChange < -0.1
+
+            # Book value is less than price
+            giveaway = (priceToBook < 1 and
+                        f2WeekChange < -0.1 and
+                        profitMargins > .1 and
+                        earningsQuarterlyGrowth > 0)
+
+            # Brightest future
+            bestfuture = (forwardEps / trailingEps > 1.25 and
+                          profitMargins > 0.1 and
+                          earningsQuarterlyGrowth > 0)
+
+            # Bright future
+            goodfuture = trailingEps <= forwardEps
+
+            # Future is dark
+            darkfuture = forwardEps < 0
+
+
+            # Sort by forwardPE and rank each company
+            spdict[s["symbol"]].append(forwardPE)
+            # Sort by PEGratio and rank each company
+            spdict[s["symbol"]].append(pegRatio)
+            # Sort by 52WeekChange and rank each company
+            spdict[s["symbol"]].append(f2WeekChange)
+            # Sort by priceToBook and rank each company
+            spdict[s["symbol"]].append(priceToBook)
+            # Sort by earningsQuarterlyGrowth and rank each company
+            spdict[s["symbol"]].append(earningsQuarterlyGrowth)
+
+            # Sum all of these up to give the total score for the ticker, then sort all by total value
+
+            header = "Symbol, Total Value, Profitable Value, Giveaway Value, Best Future Value, earningsQuarterlyGrowth, priceToBook, forwardPE, pegRatio, 52WeekChange, marketPrice, Industry, earningsDate, recommendation"
+            logging.info(header)
+
 
 
 if __name__ == "__main__":
@@ -73,7 +155,11 @@ if __name__ == "__main__":
     logger.info("==========START==========")
     with requests.Session() as sess:
         try:
-            ret = download(sess)
+            tickers = getSP500()
+            # ret = download(sess, tickers)
+            # logging.info("download() COMPLETE with ret = {}".format(ret))
+            ret = evaluate(tickers)
+            logging.info("evaluate() COMPLETE with ret = {}".format(ret))
         except Exception as e:
             logger.exception("Exception: [{}]".format(e))
     logger.info("Exiting with code = {}".format(ret))
